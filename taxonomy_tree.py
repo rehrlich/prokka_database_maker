1f6400b2b1b1d8fb4b0f8a45f9edabc41ab90bec
rachel <rehrlich7@gmail.com>
2016-06-10 16:52:27 -0400

    parses taxonomy tree file

---
+++ taxonomy_tree.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+
+class TaxonomyTree:
+    def __init__(self):
+        self.children = defaultdict(list)
+        self.nodes_file = '/opt/users/rachel/apps/taxator_refs/taxdump/nodes.dmp'
+        self.parse_tree()
+
+    def parse_tree(self):
+        with open(self.nodes_file, 'r') as f:
+            for line in f:
+                split_line = line.split('\t|\t')
+                self.children[split_line[1]].append(split_line[0])
+
+    def get_consistent_below(self, taxa):
+        all_children = set()
+        all_children.add(taxa)
+        consistent_taxa = set()
+        while all_children:
+            curr = all_children.pop()
+            consistent_taxa.add(curr)
+            all_children = all_children.union(self.children[curr])
+
+        return consistent_taxa
